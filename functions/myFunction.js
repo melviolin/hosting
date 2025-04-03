3914b65500966b518ea41275bebd40d79ee1d730
MelViolin <153835343+melviolin@users.noreply.github.com>
2025-04-03 00:41:44 -0500

    Update myFunction.js

--- functions/myFunction.js
+++ functions/myFunction.js
@@ -1,5 +1,5 @@
 export async function onRequest(context) {
-  return new Response('¡Hola desde Cloudflare Pages!', {
+  return new Response('¡Hola desde Cloudflare Pages - MelViolin!', {
     headers: { 'Content-Type': 'text/plain' },
   });
 }
