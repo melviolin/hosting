b0a3dc1d6d0c442f765082834e266fee150cf98d
MelViolin <153835343+melviolin@users.noreply.github.com>
2025-04-03 01:11:12 -0500

    Update paginas_pdf.js

--- functions/paginas_pdf.js
+++ functions/paginas_pdf.js
@@ -4,18 +4,14 @@ const pdfParse = require('pdf-parse'); // Para analizar el PDF
 module.exports = async (req, res) => {
   if (req.method === 'POST') {
     try {
-      // Extraer el enlace del PDF enviado desde el cuerpo de la solicitud
       const { pdfUrl } = req.body;
 
-      // Descargar el archivo PDF desde Google Drive
       const response = await fetch(pdfUrl);
       const pdfBuffer = await response.buffer();
 
-      // Analizar el PDF y obtener la cantidad de páginas
       const pdfData = await pdfParse(pdfBuffer);
       const numPages = pdfData.numpages;
 
-      // Retornar el número de páginas en formato JSON
       res.status(200).json({ numPages: numPages });
     } catch (error) {
       res.status(500).json({ error: error.message });
